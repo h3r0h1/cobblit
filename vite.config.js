4996accb2c421d592e0a55cb4ada301cdcb647a7
Hero Hirdes <herohirdes@gmail.com>
2026-05-07 14:13:30 +0200

    languages add

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,18 @@ export default defineConfig({
         stappenplan: resolve(__dirname, 'stappenplan.html'),
         producten: resolve(__dirname, 'producten.html'),
         contact: resolve(__dirname, 'contact.html'),
+        // English
+        'en/index': resolve(__dirname, 'en/index.html'),
+        'en/verkooppunten': resolve(__dirname, 'en/verkooppunten.html'),
+        'en/stappenplan': resolve(__dirname, 'en/stappenplan.html'),
+        'en/producten': resolve(__dirname, 'en/producten.html'),
+        'en/contact': resolve(__dirname, 'en/contact.html'),
+        // German
+        'de/index': resolve(__dirname, 'de/index.html'),
+        'de/verkooppunten': resolve(__dirname, 'de/verkooppunten.html'),
+        'de/stappenplan': resolve(__dirname, 'de/stappenplan.html'),
+        'de/producten': resolve(__dirname, 'de/producten.html'),
+        'de/contact': resolve(__dirname, 'de/contact.html'),
       },
     },
   },
